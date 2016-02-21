b905629df84b5b8e2f55af032a6a45af98d54896
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2016-02-21 15:45:22 +0200

    Use configuration file instead of commandline parameters
    * It should be a bit more secure used this way because you can control
      the access for the config file.

--- README.md
+++ README.md
@@ -4,8 +4,11 @@ A python script to update the the IP address at OVH DynHost
 ## Dependecies
 This project needs the [ipgetter](https://github.com/phoemur/ipgetter) and [requests](http://requests.readthedocs.org/en/latest/) libraries.
 
-Install with `pip`
+Install with `pip3`
 
 ```
-sudo pip install ipgetter requests
+sudo pip3 install ipgetter requests
 ```
+
+## Configuration file
+This script requires a configuration file called `config.ini`. See `config.ini.example` for configuration examples and create your own `config.ini` before running the script.
--- ovh_dynhost.py
+++ ovh_dynhost.py
@@ -3,29 +3,36 @@
 A simple script to update the OVH DynHost entry with the current external IP of this machine.
 """
 
-import argparse
+import configparser
 import ipgetter
 import requests
 import socket
 import sys
 
 def main():
-	parser = argparse.ArgumentParser(prog='ovh_dynhost', description='Updates the OVH DynHost entry to the current IP address.')
-	parser.add_argument('domain', help='The FQDN to be updated.')
-	parser.add_argument('user', help='The user id that has access to update the FQDN.')
-	parser.add_argument('password', help='The password for the user id.')
-	parser.add_argument('-ip', help='Update to this ip address instead of the current external address.', default=ipgetter.myip(), metavar='x.x.x.x')
-	args = parser.parse_args()
-	
-	domain = args.domain
-	ip = args.ip
-	user = args.user
-	password = args.password
-	
-	if not check_dns(domain, ip):
-		update_dns(domain, ip, user, password)
-	else:
-		sys.stdout.write("IP did not change.\n")
+	cp = configparser.ConfigParser()
+	if len(cp.read('config.ini')) == 0:
+		sys.stderr.write("Config file is missing or empty.\n")
+		sys.exit(-1)
+
+	for section in cp.sections():
+		domain = section
+
+		if cp.has_option(section, 'Username'):
+			username = cp.get(domain, 'Username')
+
+		if cp.has_option(section, 'Password'):
+			password = cp.get(domain, 'Password')
+
+		if cp.has_option(section, 'IP'):
+			ip = cp.get(domain, 'IP')
+		else:
+			ip = ipgetter.myip()
+
+		if not check_dns(domain, ip):
+			update_dns(domain, ip, username, password)
+		else:
+			sys.stdout.write(domain + ": IP did not change.\n")
 
 def check_dns(domain, ip):
 	addr_info = socket.getaddrinfo(domain, 80)
@@ -35,15 +42,15 @@ def check_dns(domain, ip):
 		
 	return False
 
-def update_dns(domain, ip, user, password):
+def update_dns(domain, ip, username, password):
 	queryArguments = {'system':'dyndns', 'hostname':domain, 'myip':ip}
-	response = requests.get("https://www.ovh.com/nic/update", params=queryArguments, auth=(user, password))
+	response = requests.get("https://www.ovh.com/nic/update", params=queryArguments, auth=(username, password))
 	if response.text.startswith('good ' + ip):
 			sys.stdout.write("Update to " + ip + " successful.\n")
 			sys.exit(0)
 	elif response.status_code == requests.codes.get(401):
 		# 401 Authentification failed
-		sys.stderr.write("Authentification failed. User or password is wrong.\n")
+		sys.stderr.write("Authentification failed. Username or password is wrong.\n")
 		sys.exit(-2)
 	elif response.status_code == requests.codes.get(403):
 		# 403 Forbidden
